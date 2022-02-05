95d4b24f874d3cb072aa7c09c86b494778149abe
Philip Kwesiga <phil.kwesiga@gmail.com>
2022-02-05 19:10:26 +0300

    try cluster in private subnet

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -3,7 +3,7 @@ module "eks" {
   version         = "17.24.0"
   cluster_name    = local.cluster_name
   cluster_version = "1.20"
-  subnets         = module.vpc.public_subnets
+  subnets         = module.vpc.private_subnets
 
   vpc_id = module.vpc.vpc_id
 
@@ -20,8 +20,6 @@ module "eks" {
       asg_desired_capacity          = 1
     },
   ]
-
-  workers_additional_policies = [aws_iam_policy.worker_policy.arn]
 
 }
 
