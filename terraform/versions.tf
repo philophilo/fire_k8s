5e728e81836d71e64d7c5dc47be866792ec7391d
Philip Kwesiga <phil.kwesiga@gmail.com>
2022-02-05 19:12:13 +0300

    remove helm requirement

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -24,11 +24,6 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = ">= 2.0.1"
     }
-
-    helm = {
-      source  = "hashicorp/helm"
-      version = ">= 1.0"
-    }
   }
 
   required_version = ">= 0.14"
